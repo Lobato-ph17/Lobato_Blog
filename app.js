0ddc8986f10cba6c425aae3f77ffc4e287ca3060
Pedro Lobato <lobato.ph17@gmail.com>
2026-05-26 10:37:25 -0300

    feat: cria rota about

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
 // Templating Engine
- app.use(expressLayout);
- app.set('layout', './layout/main');
- app.set('view engine', 'ejs');
+app.use(expressLayout);
+app.set("layout", "layouts/main");
+app.set("view engine", "ejs");
 
-app.use(express.static('public')); 
+app.use(express.static("public"));
 
-app.use('/', require('./server/routes/main'));
+app.use("/", require("./server/routes/main"));
 
-app.listen(PORT, ()=> {
-    console.log(`App listening on port: ${PORT}` ); 
-}) 
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`);
+});
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,11 +1,13 @@
-const express = require('express'); 
+const express = require("express");
 const router = express.Router();
 
-
 //Router
-router.get('', (req, res) => {
-    res.send("Hello world");
-})
+router.get("", (req, res) => {
+  res.render("index");
+});
 
+router.get("/about", (req, res) => {
+  res.render("about");
+});
 
 module.exports = router;
